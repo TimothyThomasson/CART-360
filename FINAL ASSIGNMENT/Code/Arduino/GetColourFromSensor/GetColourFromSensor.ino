0c392ab908edde58fbddbddd672899b5c171afb3
TimothyThomasson <timothythomasson@gmail.com>
2017-12-07 23:36:17 -0500

    uploads

---
+++ FINAL ASSIGNMENT/Code/Arduino/GetColourFromSensor/GetColourFromSensor.ino
@@ -0,0 +1,63 @@
+// Timothy Thomasson - Colour Correct 
+// This project makes use of the AdaFruit TCS34725 color sensor Library located at https://learn.adafruit.com/adafruit-color-sensors/overview 
+
+//-----------------------------------------------------------------------------------------------------//
+
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+// Pick analog outputs, for the UNO these three work well
+#define redpin 3
+#define greenpin 5
+#define bluepin 6
+// for a common anode LED, connect the common pin to +5V
+// for common cathode, connect the common to ground
+
+
+// our RGB -> eye-recognized gamma color
+byte gammatable[256];
+
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+void setup() {
+  Serial.begin(9600);
+  if (tcs.begin()) {
+
+  // thanks PhilB for this gamma table!
+  // it helps convert RGB colors to what humans see
+  for (int i = 0; i < 256; i++) {
+    float x = i;
+    x /= 255;
+    x = pow(x, 2.5);
+    x *= 255;
+
+    if (commonAnode) {
+      gammatable[i] = 255 - x;
+    } else {
+      gammatable[i] = x;
+    }
+  }
+}
+
+void loop() {
+  uint16_t clear, red, green, blue;
+
+  delay(60);  // Delay 
+
+  tcs.getRawData(&red, &green, &blue, &clear);
+
+  tcs.setInterrupt(true);  // turn off LED
+
+  Serial.print("C:\t"); Serial.print(clear);
+  Serial.print("\tR:\t"); Serial.print(red);
+  Serial.print("\tG:\t"); Serial.print(green);
+  Serial.print("\tB:\t"); Serial.print(blue);
+
+  //Serial.print((int)r ); Serial.print(" "); Serial.print((int)g);Serial.print(" ");  Serial.println((int)b );
+
+  analogWrite(redpin, gammatable[(int)r]);
+  analogWrite(greenpin, gammatable[(int)g]);
+  analogWrite(bluepin, gammatable[(int)b]);
+}
+
